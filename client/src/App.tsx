b1d782e1b065b74f03ec8d9b09973589c4d2c0cc
Jayesh Bhole <jayeshbhole123@gmail.com>
2022-08-01 09:35:22 +0530

    feat: client moralis init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,27 +2,41 @@ import * as React from "react";
 import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
+import { Suspense, lazy, createElement, useEffect } from "react";
+import { useMoralis } from "react-moralis";
 
-export const App = () => (
-    <ChakraProvider theme={theme}>
-        <Box textAlign="center" fontSize="xl">
-            <Grid minH="100vh" p={3}>
-                <ColorModeSwitcher justifySelf="flex-end" />
-                <VStack spacing={8}>
-                    <Logo h="40vmin" pointerEvents="none" />
-                    <Text>
-                        Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-                    </Text>
-                    <Link
-                        color="teal.500"
-                        href="https://chakra-ui.com"
-                        fontSize="2xl"
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        Learn Chakra
-                    </Link>
-                </VStack>
-            </Grid>
-        </Box>
-    </ChakraProvider>
-);
+export const App = () => {
+    const { isWeb3Enabled, enableWeb3, isAuthenticated, isWeb3EnableLoading } = useMoralis();
+
+    useEffect(() => {
+        const connectorId = window.localStorage.getItem("connectorId");
+        if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading)
+            // @ts-ignore
+            enableWeb3({ provider: connectorId });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isAuthenticated, isWeb3Enabled]);
+
+    return (
+        <ChakraProvider theme={theme}>
+            <Box textAlign="center" fontSize="xl">
+                <Grid minH="100vh" p={3}>
+                    <ColorModeSwitcher justifySelf="flex-end" />
+                    <VStack spacing={8}>
+                        <Logo h="40vmin" pointerEvents="none" />
+                        <Text>
+                            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+                        </Text>
+                        <Link
+                            color="teal.500"
+                            href="https://chakra-ui.com"
+                            fontSize="2xl"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Learn Chakra
+                        </Link>
+                    </VStack>
+                </Grid>
+            </Box>
+        </ChakraProvider>
+    );
+};
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,15 +4,37 @@ import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import { MoralisProvider } from "react-moralis";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
+const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
+const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+const Application = () => {
+    const isServerInfo = APP_ID && SERVER_URL ? true : false;
+    //Validate
+    if (!APP_ID || !SERVER_URL)
+        throw new Error(
+            "Missing Moralis Application ID or Server URL. Make sure to set your .env file."
+        );
+    if (isServerInfo)
+        return (
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <App />
+            </MoralisProvider>
+        );
+    else {
+        return <div style={{ display: "flex", justifyContent: "center" }}>Moralis Init Error</div>;
+    }
+};
+
 root.render(
     <React.StrictMode>
         <ColorModeScript />
-        <App />
+        <Application />,
     </React.StrictMode>
 );
 
