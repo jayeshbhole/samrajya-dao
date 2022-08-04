6896856baff4fb27edf68d262ac7fa59c6836478
Jayesh Bhole <jayeshbhole123@gmail.com>
2022-08-05 04:02:58 +0530

    feat: fork dao-template

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,110 +1,64 @@
-import * as dotenv from "dotenv";
-import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig, task, types } from "hardhat/config";
-import "@nomiclabs/hardhat-ethers";
-import "hardhat-deploy";
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
 
-dotenv.config();
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
-
-    for (const account of accounts) {
-        console.log(account.address);
-    }
-});
-
-const getSecret = (name: string): string => {
-    return process.env?.[name] ?? "";
-};
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "privatKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 const config: HardhatUserConfig = {
-    solidity: {
-        version: "0.8.4",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200,
-            },
-        },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
     },
-    networks: {
-        localhost: {
-            allowUnlimitedContractSize: true,
-            url: "http://localhost:8545",
-            accounts: [getSecret("PRIVATE_KEY"), getSecret("PRIVATE_KEY2")],
-            saveDeployments: false,
-        },
-        hardhat: {
-            chainId: 31337,
-            allowUnlimitedContractSize: true,
-            forking: {
-                enabled: true,
-                url: getSecret("MUMBAI_URL"),
-                blockNumber: 27004544,
-            },
-            mining: {
-                auto: true,
-                interval: 0,
-            },
-            accounts: [
-                {
-                    privateKey: getSecret("PRIVATE_KEY"),
-                    balance: "10000000000000000000000000000000000000000000000000",
-                },
-                {
-                    privateKey: getSecret("PRIVATE_KEY2"),
-                    balance: "1000000000000000000000",
-                },
-            ],
-        },
-
-        goerli: {
-            url: getSecret("GOERLI_URL"),
-            accounts: getSecret("PRIVATE_KEY") !== undefined ? [getSecret("PRIVATE_KEY")] : [],
-            verify: {
-                etherscan: {
-                    apiKey: getSecret("ETHERSCAN_API_KEY"),
-                },
-            },
-        },
-
-        rinkeby: {
-            url: getSecret("RINKEBY_URL"),
-            accounts: getSecret("PRIVATE_KEY") !== undefined ? [getSecret("PRIVATE_KEY")] : [],
-            verify: {
-                etherscan: {
-                    apiKey: getSecret("ETHERSCAN_API_KEY"),
-                },
-            },
-        },
-
-        mumbai: {
-            url: getSecret("MUMBAI_URL"),
-            accounts: getSecret("PRIVATE_KEY") !== undefined ? [getSecret("PRIVATE_KEY")] : [],
-            verify: {
-                etherscan: {
-                    apiKey: getSecret("POLYSCAN_API_KEY"),
-                },
-            },
-        },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
     },
-    etherscan: {
-        apiKey: {
-            mainnet: getSecret("ETHERSCAN_API_KEY"),
-            goerli: getSecret("ETHERSCAN_API_KEY"),
-            polygonMumbai: getSecret("POLYSCAN_API_KEY"),
-            polygon: getSecret("POLYSCAN_API_KEY"),
-        },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-        user: {
-            default: 1,
-        },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
     },
-};
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+}
 
-export default config;
+export default config
