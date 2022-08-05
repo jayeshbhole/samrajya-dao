0619c0dcc39cfbc5224265f9a987b53e59d9518a
Jayesh Bhole <jayeshbhole123@gmail.com>
2022-08-05 16:17:19 +0530

    chore: run prettier

--- contracts/utils/move-time.ts
+++ contracts/utils/move-time.ts
@@ -1,8 +1,8 @@
 import { network } from "hardhat"
 
 export async function moveTime(amount: number) {
-  console.log("Moving blocks...")
-  await network.provider.send("evm_increaseTime", [amount])
+    console.log("Moving blocks...")
+    await network.provider.send("evm_increaseTime", [amount])
 
-  console.log(`Moved forward in time ${amount} seconds`)
+    console.log(`Moved forward in time ${amount} seconds`)
 }
