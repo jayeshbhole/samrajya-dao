2700153e74220cb31434f0e803956b34f0d93ce2
Jayesh Bhole <jayeshbhole123@gmail.com>
2022-08-05 16:02:54 +0530

    chore: run prettier

--- contracts/contracts/Box.sol
+++ contracts/contracts/Box.sol
@@ -5,19 +5,19 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Box is Ownable {
-  uint256 private value;
+    uint256 private value;
 
-  // Emitted when the stored value changes
-  event ValueChanged(uint256 newValue);
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
 
-  // Stores a new value in the contract
-  function store(uint256 newValue) public onlyOwner {
-    value = newValue;
-    emit ValueChanged(newValue);
-  }
+    // Stores a new value in the contract
+    function store(uint256 newValue) public onlyOwner {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
 
-  // Reads the last stored value
-  function retrieve() public view returns (uint256) {
-    return value;
-  }
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
 }
